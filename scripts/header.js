c62f33b317fdd6aa3d188d35c0751c10af34287d
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-02-21 20:11:26 +0530

    fix: change class name to hyphen notation class names

--- scripts/header.js
+++ scripts/header.js
@@ -1,17 +1,17 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
     menu.style.display = "none";
   } else {
-    menu.classList.add("showMenu");
+    menu.classList.add("show-menu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
     menu.style.display = "block";
